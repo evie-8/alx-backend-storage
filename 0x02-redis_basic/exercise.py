04ea0547315e2d611e897d89016c47ec6168c771
evie-8 <neouma29@gmail.com>
2024-04-05 17:49:08 +0300

    corrections

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,6 +25,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''generates a key and stores input data'''
         key = str(uuid.uuid4())
