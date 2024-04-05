37167acbcc888b87b5b172d997c0fd711c5f74d7
evie-8 <neouma29@gmail.com>
2024-04-05 18:02:11 +0300

    corrections

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,6 +19,46 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+
+def call_history(method: Callable) -> Callable:
+    '''tore the history of inputs and
+    outputs for a particular function.'''
+    
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        ''' def wrapper'''
+        key_m = method.__qualname__
+        inp = key_m + ':inputs'
+        out = key_m + ":outputs"
+        data = str(args)
+        self._redis.rpush(inp, data)
+        fin = method(self, *args, **kwargs)
+        self._redis.rpush(out, str(fin))
+        return fin
+    return wrapper
+
+
+def replay(func: Callable):
+    '''to display the history of calls of a particular function.'''
+    red = redis.Redis()
+    key = func.__qualname__
+    inp = red.lrange("{}:inputs".format(key), 0, -1)
+    out = red.lrange("{}:outputs".format(key), 0, -1)
+    calls_number = len(inp)
+    times_str = 'times'
+    if calls_number == 1:
+        times_str = 'time'
+    fin = '{} was called {} {}:'.format(key, calls_number, times_str)
+    print(fin)
+    for k, v in zip(inp, out):
+        fin = '{}(*{}) -> {}'.format(
+            key,
+            k.decode('utf-8'),
+            v.decode('utf-8')
+        )
+        print(fin)
+
+
 class Cache():
     '''class Cache'''
     def __init__(self):
@@ -26,6 +66,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''generates a key and stores input data'''
         key = str(uuid.uuid4())
