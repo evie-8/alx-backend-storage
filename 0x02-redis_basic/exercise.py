90df1c5a6f4f875a0bbee5ee674ea9034dee9764
evie-8 <neouma29@gmail.com>
2024-04-05 17:34:51 +0300

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache():
@@ -18,3 +18,18 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+     def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        ''' def get and fn which converts bakc to desired format'''
+        value = self._redis.get(key)
+        return value if not fn else fn(value)
+
+    def get_int(self, key):
+        '''returns int value'''
+        return self.get(key, int)
+
+    def get_str(self, key):
+        '''returns string value'''
+        value = self._redis.get(key)
+        return value.decode("utf-8")
