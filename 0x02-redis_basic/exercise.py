e58f6ea0ac9511719b5e3460bd9937c87284bbc9
evie-8 <neouma29@gmail.com>
2024-04-05 18:37:44 +0300

    count-call

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,20 @@ from typing import Union, Callable, Optional
 from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    """akes a single method Callable argument
+    and returns a Callable."""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
 class Cache():
     '''class Cache'''
     def __init__(self):
@@ -14,6 +28,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''generates a key and stores input data'''
         key = str(uuid.uuid4())
